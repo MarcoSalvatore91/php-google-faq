f6c4709f03199e075eae547432e6c18e4275b9cf
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-28 17:36:30 +0100

    added section top-header

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+$navbars = [
+    'Introduzione',
+    'Norme sulla privacy',
+    'Termini di servizio',
+    'Tecnologie',
+    'Domande Frequenti',
+];
+
 $faqs = [
 
     'law' => [
@@ -119,6 +127,7 @@ $faqs = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <title>FAQ</title>
@@ -127,7 +136,16 @@ $faqs = [
 <body>
 
     <header>
-
+        <div class="d-flex justify-content-between">
+            <div class="d-flex">
+                <img class="me-2" src="img/Google_2015_logo.svg.png" alt="logo">
+                <h4>Privacy e termini</h4>
+            </div>
+            <div>
+                <i class="fa-solid fa-bars"></i>
+                <span>M</span>
+            </div>
+        </div>
     </header>
 
     <section class="container-m">
